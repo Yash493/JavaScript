c721d5c02ce0e26b02ea3b44201d473fd85a93ec
Yash Gaikwad <yashgaikwad493@gmail.com>
2025-05-09 19:09:19 +0530

    varibales-1

---
+++ Basics/Variables.js
@@ -0,0 +1,20 @@
+const accountId= 1231;
+
+let accountEmail="yashgaikwad493@gmail.com";
+
+
+var accoountPass="12345";
+
+accountCity="Nashik"
+
+
+//accountId=2;
+
+accountEmail="yash#.come";
+accoountPass="234";
+accountCity="Mumbai";
+console.log(accountEmail);
+console.log(accoountPass);
+console.log(accountCity);
+
+console.table([accountId,accountEmail,accountEmail,accountCity]);
