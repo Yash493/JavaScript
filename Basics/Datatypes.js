561e5f1f315c6a1f3a957b3e44fe282851e0b5b3
Yash Gaikwad <yashgaikwad493@gmail.com>
2025-05-09 19:23:51 +0530

    Datatypes-2

---
+++ Basics/Datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(2+2) //we are using node.js not browser so we will get error
+
+//console.log(2+2);
+
+
+// Datatypes in javascript
+
+//1. number == range 2 to power of 53
+// 2. bigInt = for large numbers
+// 3. string =="" 
+// 4. boolean => true/false
+// 5. null === it is also standalone type 
+// 6. undefined == when value id not defined
+// 7. symbol= it is used to find uniqueness 
+
+
+// 8. object
+console.log(typeof undefined); //output: undefind
+console.log(typeof null);// output: object
+
+
